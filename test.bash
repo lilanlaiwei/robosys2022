b5f3e4b7b0fdff944b016095a1e01ba7f6f19a02
Riran Seo <seoriran@DESKTOP-9GO9NP0.localdomain>
2022-10-26 19:09:50 +0900

    Delete

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Riran Seo
 # SPDX-License-Indentifier: BSD-3-Clause
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus_stdin)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ |./plus_stdin)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus_stdin) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = " "] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
